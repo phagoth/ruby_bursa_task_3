be55a99283627c9ab6b273bc3ffbae717a2de8c2
Petro Koriakin <pkor@softserveinc.com>
2015-07-04 15:51:49 +0300

    add some complexity

--- library_manager_spec.rb
+++ library_manager_spec.rb
@@ -7,7 +7,8 @@ describe LibraryManager do
 Hello, Ivan Testenko!
 
 You should return a book "War and Peace" authored by Leo Tolstoy in 36 hours.
-Otherwise you will be charged $12.3 code per hour. 
+Otherwise you will be charged $12.3 per hour.
+By the way, you are on 333 page now and you need 5.4 hours to finish reading "War and Peace"
 TEXT
   end
 
@@ -18,9 +19,9 @@ Hello,
 There are 5 published books in the library.
 There are 6 readers and 3 of them are reading the books.
 
-Ivan Testenko is reading "War and Peace" - should return on 2015-07-04 at 10am
-Vasiliy Pupkin is reading "Red and Black" - should return on 2015-07-12 at 7pm
-Barak Obama is reading "Well grounded Rubyist" - should return on 2015-07-10 at 12pm
+Ivan Testenko is reading "War and Peace" - should return on 2015-07-04 at 10am - 5.0 hours of reading is needed to finish.
+Vasiliy Pupkin is reading "Red and Black" - should return on 2015-07-12 at 7pm  - 12.7 hours of reading is needed to finish.
+Barak Obama is reading "The Well-Grounded Rubyist" - should return on 2015-07-10 at 12pm  - 44.5 hours of reading is needed to finish.
 TEXT
   end
 
@@ -32,9 +33,9 @@ The library has:
 - 5 books
 - 4 authors
 - 6 readers
-The most popular author is Leo Tolstoy - 2450 pages has been read in 2 books by 4 readers.
-The most productive reader is Ivan Testenko - he had read 1040 pages in 3 books authored by 3 authors.
-
+The most popular author is Leo Tolstoy: 2450 pages has been read in 2 books by 4 readers.
+The most productive reader is Ivan Testenko: he had read 1040 pages in 3 books authored by 3 authors.
+The most popular book is "The Well-Grounded Rubyist" authored by David A. Black: it had been read for 123.0 hours by 5 readers.
 TEXT
   end
   
